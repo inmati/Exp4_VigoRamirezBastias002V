c4aeb41c6e62722a61029220551a19876c0d07e2
mativimu <mativimu.dev@gmail.com>
2022-05-25 18:48:11 -0400

    Exp2_BastiasVigo_002V

--- js/validation.js
+++ js/validation.js
@@ -30,7 +30,58 @@ $(document).ready(
                 direccion: {
                     required: 'Ingrese una dirección',
                 }
-            }//messages
-        }); //$('#mi-formulario').validate
+            }
+        }); $('#registrate').validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                nombre: "required",
+                apellido: "required",
+                fono: "required",
+                direccion: "required",
+                username: "required",
+                password: {
+                    required: true
+                },
+                password2: {
+                    required: true,
+                    equalTo: '#pass'
+                }
+            },
+            messages: {
+                email: {
+                    required: 'Ingresa tu correo electrónico',
+                    email: 'Formato de correo no válido'
+                },
+                nombre: {
+                    required: 'Ingrese un nombre',
+                },
+                apellido: {
+                    required: 'Ingrese un apellido',
+                },
+                fono: {
+                    required: 'Ingrese un número de celular',
+                    minlength: 'Cantidad de digitos insuficiente'
+                },
+                direccion: {
+                    required: 'Ingrese una dirección',
+                },
+                username: {
+                    required: 'Ingrese un apellido',
+                },
+                password: {
+                    required: 'Ingresa una contraseña',
+                    minlength: 'Caracteres insuficientes'
+                },
+                password2:{
+                    required: 'Ingresa una contraseña',
+                    equalTo: 'Las contraseñas no coinciden',
+                    minlength: 'Caracteres insuficientes'
+                }
+            }
+
+        });
     })
 );
